9c568b6453a79d05c6ccb15a5ca00ad31b29711d
BarbaraJulieth <barbaratv2000@gmail.com>
2021-12-03 18:42:55 -0500

    version 3.0.0 de Alquil.app en Angular

--- src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import * as cryptoJS from 'crypto-js';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 @Component({
   selector: 'app-identificacion',
   templateUrl: './identificacion.component.html',
@@ -13,7 +15,9 @@ export class IdentificacionComponent implements OnInit {
     'clave':['',[Validators.required]]
   });
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,
+    private servicioSeguridad: SeguridadService,
+    private router: Router) { }
 
   ngOnInit(): void {
 
@@ -21,9 +25,14 @@ export class IdentificacionComponent implements OnInit {
   IdentificarUsuario(){
     let usuario =this.fgValidador.controls['usuario'].value;
     let clave = this.fgValidador.controls['clave'].value;
-    alert (usuario);
-    alert(clave)
-
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+    this.servicioSeguridad.Identificar(usuario,claveCifrada).subscribe((datos:any)=>{
+      //OK
+      this.servicioSeguridad.AlmacenarSesion(datos);
+      this.router.navigate(["/inicio"]);
+    }, (error:any)=> {
+      alert("El usuario o contraseña esta incorrecta")
+    })
   }
   
 
